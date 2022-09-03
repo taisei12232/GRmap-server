36cf10a9251a6bd6e86b879f0c70faa6c253edee
taisei12232 <taisei12232@gmail.com>
2022-09-03 22:59:15 +0900

    init api endpoint

--- main.py
+++ main.py
@@ -10,11 +10,26 @@ app = FastAPI()
 def root():
     return {"Hello": "World"}
 
+@app.get("/reviews")
+def getreview():
+    return 200
+
+@app.post("/review")
+def postreview():
+    return 200
+
+@app.get("/location")
+def getfest():
+    return 200
+
+@app.get("/reviewimage")
+def geturl():
+    return 200
 
 def main() -> None:
     print("===== main() =====")
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True, workers=2)
+    uvicorn.run("main:app", host="0.0.0.0", port=8080, reload=True, workers=2)
 
 
 if __name__ == "__main__":
-    main()
+    main() 
